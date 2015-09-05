1cad9d8048dff7c6d26bd1a78053884b62d6f2a7
Marco Zanini <chetto87@gmail.com>
2015-09-05 16:32:56 +0200

    Use content_tag in application.erb and add assertion for error messages on signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,19 +2,22 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 	
-	test "invalid signup information" do
+	test "If passwords do not match user is not added" do
 		get signup_path
 		assert_no_difference 'User.count' do 
 			post users_path, user:{ name: "", email: "user@invalid.com", password:"foo", password_confirmation: "bar" }
 		end
 		assert_template 'users/new'
+		assert_select 'div[id=error_explanation]'
+		assert_select 'div[class="alert alert-danger"]'
 	end
 
-	test "valid signup information" do
+	test "if valid signup information user is registered" do
 		get signup_path
 		assert_difference 'User.count', 1 do
 			post_via_redirect users_path, user:{ name: "Marco", email: "user@invalid.com", password:"foobar", password_confirmation: "foobar" }
 		end
 		assert_template 'users/show'
+		assert_not flash.empty?
 	end
 end
